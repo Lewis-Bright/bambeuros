00190dfe7b4c3d82184fde2832312a7d20e15863
Lewis.Bright <sarcasticGamesOrg@gmail.com>
2019-09-18 21:32:20 +0100

    Spring Boot Initializr

---
+++ src/main/java/com/lewisb/bambeuro/BambeuroApplication.java
@@ -0,0 +1,13 @@
+package com.lewisb.bambeuro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambeuroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambeuroApplication.class, args);
+	}
+
+}
