cb9c48f96965398700b39d29711730bf3e4a24f6
Lewis.Bright <sarcasticGamesOrg@gmail.com>
2019-09-20 07:18:03 +0100

    Logout button

--- bambeuro-ui/src/components/UserSummary.js
+++ bambeuro-ui/src/components/UserSummary.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import LoginRedirect from "./LoginRedirect";
 
 class UserSummary extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userInfo: ""
+      userInfo: "",
+      isLoggingOut: false
     };
   }
   componentDidMount() {
@@ -14,11 +16,21 @@ class UserSummary extends Component {
         this.setState({ userInfo: userInfo });
       });
   }
+  handleClick = () => {
+    this.setState({
+      isLoggingOut: true
+    });
+  };
+
   render() {
+    if (this.state.isLoggingOut === true) {
+      return <LoginRedirect />;
+    }
     return (
       <div>
         <h1>User Summary:</h1>
         <p>{this.state.userInfo}</p>
+        <button onClick={this.handleClick}>Logout</button>
       </div>
     );
   }
